da6a869347d2eff66b262efec1828dc9216e9582
ZenTBelL <peerasak.sane@hotmail.com>
2017-02-14 12:59:34 +0700

    update docker compose to v 1.11.1

--- docker-install/install-docker-compose-on-ubuntu16.sh
+++ docker-install/install-docker-compose-on-ubuntu16.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/run.sh" \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.11.1/run.sh" \
   > /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
